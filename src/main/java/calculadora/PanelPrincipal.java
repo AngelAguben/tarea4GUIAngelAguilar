545a9c429bbf61afd24d210df1df872d287b7a4c
angel <angelbmx002@gmail.com>
2021-03-24 14:22:29 +0100

    Comienza el proyecto

---
+++ src/main/java/calculadora/PanelPrincipal.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author angel
+ */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+/**
+ * @author Juan Carlos Fernández Vico
+ */
+public class PanelPrincipal extends JPanel implements ActionListener {
+
+    // Atributos de la clase (privados)
+    private PanelBotones botonera;
+    private JTextArea areaTexto;
+    private int tipoOperacion;
+    private static ArrayList<String> operandos = new ArrayList<>();
+    private static String texto = "";
+
+    // Constructor
+    public PanelPrincipal() {
+        initComponents();
+        tipoOperacion = -1; // No hay operaciones en la calculadora
+    }
+
+    // Se inicializan los componentes gráficos y se colocan en el panel
+    private void initComponents() {
+        // Creamos el panel de botones
+        botonera = new PanelBotones();
+        // Creamos el área de texto
+        areaTexto = new JTextArea(10, 50);
+        areaTexto.setEditable(false);
+        areaTexto.setBackground(Color.white);
+
+        //Establecemos layout del panel principal
+        this.setLayout(new BorderLayout());
+        // Colocamos la botonera y el área texto
+        this.add(areaTexto, BorderLayout.NORTH);
+        this.add(botonera, BorderLayout.SOUTH);
+        for (JButton boton : this.botonera.getgrupoBotones()) {
+            boton.addActionListener(this);
+        }
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+
+        // Se obtiene el objeto que desencadena el evento
+        Object o = ae.getSource();
+        // Si es un botón
+        if (o instanceof JButton) {
+            System.out.println(((JButton) o).getText());
+            texto += ((JButton) o).getText();
+            areaTexto.setText(texto);
+            System.out.println("TT " + texto);
+//            areaTexto.setText(((JButton) o).getText());
+
+            operandos.add(((JButton) o).getText());
+
+        }
+//        for (String operando : operandos) {
+//            System.out.println("Opera " + operando);
+//        }
+
+    }
+}
